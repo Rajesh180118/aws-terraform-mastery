332b4d7c17590c0ba6437125b0a10a76393bd9b5
Rajesh180118 <rajeshbarik1801@gmail.com>
2025-11-26 16:53:42 +0530

    DAY3

---
+++ DAY3/main.tf
@@ -0,0 +1,34 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
+  }
+}
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+#Create a VPC
+resource "aws_vpc" "demo-vpc" {
+  cidr_block = "10.0.0.0/28"
+  tags = {
+    Name = "demo-vpc"
+  }
+}
+
+# Create a simple S3 bucket
+resource "aws_s3_bucket" "first_bucket" {
+  bucket = "mys3bucketbyraja1801-${aws_vpc.demo-vpc.id}"
+
+  tags = {
+    Name   = "My bucket"
+  }
+}
