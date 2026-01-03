685c060429caa4ef47fa47f54b37ddd760771885
Rajesh180118 <rajeshbarik1801@gmail.com>
2026-01-03 20:50:23 +0530

    DAY17

---
+++ DAY17/variables.tf
@@ -0,0 +1,28 @@
+variable "app_name" {
+  description = "Name of the Elastic Beanstalk application"
+  type        = string
+  default     = "my-app-bluegreen"
+}
+
+variable "solution_stack_name" {
+  description = "Elastic Beanstalk solution stack name (platform)"
+  type        = string
+  # Node.js 20 running on 64bit Amazon Linux 2023
+  default = "64bit Amazon Linux 2023 v6.7.1 running Node.js 20"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type for Elastic Beanstalk environments"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "tags" {
+  description = "Tags to apply to all resources"
+  type        = map(string)
+  default = {
+    Project     = "BlueGreenDeployment"
+    Environment = "Demo"
+    ManagedBy   = "Terraform"
+  }
+}
